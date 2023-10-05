1ec13ac8d0885ef19d3eafd27cd419eaef777f7d
reza3524 <shams.reza1375@gmail.com>
2023-10-05 21:58:37 +0330

    Initialized commit.

---
+++ src/main/java/com/reza/discoveryserver/configuration/EurekaServerConfig.java
@@ -0,0 +1,9 @@
+package com.reza.discoveryserver.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfig {
+}
